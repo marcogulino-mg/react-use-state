a4e1b2afebf6f4e4351cb25252ad02d766fe9ad8
marco-gulino <marcogulino98@gmail.com>
2025-02-17 18:37:18 +0100

    [feat] created macro components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Hello from "./components/Counter.jsx";
+// Import HEADER
+import Header from "./components/Header.jsx";
+// Import MAIN
+import Main from "./components/Main.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>Main Content</h2>
+    </main>
+  );
+}
